5e933fa8869436fd602e9c57068068c68e77861e
RisSonthalia <154369178+RisSonthalia@users.noreply.github.com>
2024-06-16 00:50:19 +0530

    Update forms.py

--- NewsApp/forms.py
+++ NewsApp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+from .models import Review
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
@@ -23,3 +24,10 @@ class SignUpForm(UserCreationForm):
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'autofocus': True}))
     password = forms.CharField(label="Password", strip=False, widget=forms.PasswordInput)
+
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['stars', 'review_text']
