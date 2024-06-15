54841ea106026d7e175fb512cbab060b6c528ebc
RisSonthalia <154369178+RisSonthalia@users.noreply.github.com>
2024-06-16 00:49:51 +0530

    Update admin.py

--- NewsApp/admin.py
+++ NewsApp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from NewsApp.models import SearchQueries
-# Register your models here.
-admin.site.register(SearchQueries)
+from .models import SearchQueries,Review
+
+class SearchQueriesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'keyword', 'positive_articles', 'neutral_articles', 'negative_articles', 'search_date')
+    search_fields = ('user__username', 'keyword')
+    list_filter = ('search_date',)
+
+admin.site.register(SearchQueries, SearchQueriesAdmin)
+admin.site.register(Review)
