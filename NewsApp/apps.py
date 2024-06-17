f1e7cb302bbd3795bad515ba40118e6c0fbd7e88
RisSonthalia <154369178+RisSonthalia@users.noreply.github.com>
2024-06-17 17:17:29 +0530

    Update apps.py

--- NewsApp/apps.py
+++ NewsApp/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class NewsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'NewsApp'
+    def ready(self):
+        import NewsApp.signals
